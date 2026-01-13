057134362e5da8710ed5e5ec35ec98c76329863f
Vsevolod Oplachko <oplachko2003@gmail.com>
2026-01-13 11:14:42 +0500

    Initial Commit

---
+++ ComparePropertyWrappers/ComparePropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComparePropertyWrappersApp.swift
+//  ComparePropertyWrappers
+//
+//  Created by Vsevolod Oplachko on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ComparePropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
